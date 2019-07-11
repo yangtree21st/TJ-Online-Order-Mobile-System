fd0703045d6ee9db879043e10adc281fd24aa5e6
Yang Shu <yangtree21st@gmail.com>
2019-07-11 16:01:34 -0400

    finish CategoryServiceImplTest

--- src/test/java/com/tjexpress/sell/service/impl/CategoryServiceImplTest.java
+++ src/test/java/com/tjexpress/sell/service/impl/CategoryServiceImplTest.java
@@ -8,6 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
@@ -19,21 +22,29 @@ public class CategoryServiceImplTest {
     private CategoryServiceImpl categoryServiceIml;
 
     @Test
-    public void findOne() {
+    public void findOne() throws Exception {
         ProductCategory productCategory = categoryServiceIml.findOne(2);
 
         Assert.assertEquals(new Integer(2),productCategory.getCategoryId());
     }
 
     @Test
-    public void findAll() {
+    public void findAll() throws Exception {
+        List<ProductCategory> list = categoryServiceIml.findAll();
+
+        Assert.assertNotEquals(0,list.size());
     }
 
     @Test
-    public void findByCategoryIn() {
+    public void findByCategoryIn() throws Exception {
+        List<ProductCategory> list = categoryServiceIml.findByCategoryIn(Arrays.asList(1,2,3,4));
+        Assert.assertNotEquals(0,list);
     }
 
     @Test
-    public void save() {
+    public void save() throws Exception {
+        ProductCategory productCategory = new ProductCategory(" test category",10);
+        ProductCategory result = categoryServiceIml.save(productCategory);
+        Assert.assertEquals(result,productCategory);
     }
 }
